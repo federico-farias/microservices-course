b40a82256b29ec7326eaf84b7c22b73fd994f5bb
Federico Farias <federico.farias@outlook.com>
2023-10-31 23:22:25 -0600

    Front Office - Se integra anotaciones de monitore

--- temaX_monitoring/ecommerce-frontoffice-orders/src/main/java/com/bintics/orders/controller/OrdersController.java
+++ temaX_monitoring/ecommerce-frontoffice-orders/src/main/java/com/bintics/orders/controller/OrdersController.java
@@ -1,5 +1,7 @@
 package com.bintics.orders.controller;
 
+import co.elastic.apm.api.CaptureSpan;
+import co.elastic.apm.api.CaptureTransaction;
 import com.bintics.orders.dto.AddProductsToOrder;
 import com.bintics.orders.dto.OrderRequest;
 import com.bintics.orders.service.OrdersService;
@@ -17,24 +19,32 @@ public class OrdersController {
     private final OrdersService service;
 
     @PostMapping
+    @CaptureSpan
+    @CaptureTransaction
     public ResponseEntity create(@RequestBody OrderRequest request) {
         var id = this.service.create(request);
         return ResponseEntity.created(URI.create(String.format("/orders/%s", id))).build();
     }
 
     @GetMapping
+    @CaptureSpan
+    @CaptureTransaction
     public ResponseEntity getAll() {
         var list = this.service.getAllOrders();
         return ResponseEntity.ok(list);
     }
 
     @GetMapping("/{orderId}/products")
+    @CaptureSpan
+    @CaptureTransaction
     public ResponseEntity getProductsByOrder(@PathVariable("orderId") String orderId) {
         var products = this.service.getProductsByOrder(orderId);
         return ResponseEntity.ok(products);
     }
 
     @PutMapping("/{orderId}/products")
+    @CaptureSpan
+    @CaptureTransaction
     public ResponseEntity addProduct(@PathVariable("orderId") String orderId, @RequestBody AddProductsToOrder request) {
         request.setOrderId(orderId);
         this.service.addProductsToOrder(request);
@@ -42,6 +52,8 @@ public class OrdersController {
     }
 
     @DeleteMapping("/{id}")
+    @CaptureSpan
+    @CaptureTransaction
     public ResponseEntity update(@PathVariable("id") String id) {
         this.service.delete(id);
         return ResponseEntity.noContent().build();
