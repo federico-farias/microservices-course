f11932b6c80e9f19c684846de1195a73afd0653b
Federico Farias <federico.farias@outlook.com>
2023-11-23 19:58:19 -0600

    Retry exponential back-off

--- tema03/ecommerce-frontoffice-orders/src/main/java/com/bintics/orders/gateway/ProductsRetryer.java
+++ tema03/ecommerce-frontoffice-orders/src/main/java/com/bintics/orders/gateway/ProductsRetryer.java
@@ -7,10 +7,18 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ProductsRetryer implements Retryer {
 
+    private final int WAIT_TIME = 1000;
+    private final int MAX_NUM_ATTEMPS = 5;
+    private int count = 0;
+
     @Override
     public void continueOrPropagate(RetryableException e) {
         try {
-            Thread.sleep(2000L);
+            this.count++;
+            Thread.sleep(WAIT_TIME * count); // Exponential back-off
+            if (this.count == MAX_NUM_ATTEMPS) {
+                throw new ProductsGatewayException("the maximum number of retries has been reached");
+            }
         } catch (InterruptedException ex) {
             Thread.currentThread().interrupt();
             throw e;
